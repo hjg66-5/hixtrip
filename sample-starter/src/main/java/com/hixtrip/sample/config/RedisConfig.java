2a1edcef0d86822e97150af720ba5a45040d4460
20322 <2032270776@qq.com>
2024-04-12 01:03:19 +0800

    redis测试

--- sample-starter/src/main/java/com/hixtrip/sample/config/RedisConfig.java
+++ sample-starter/src/main/java/com/hixtrip/sample/config/RedisConfig.java
@@ -1,11 +1,17 @@
 package com.hixtrip.sample.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.*;
+
+import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
@@ -31,5 +37,30 @@ public class RedisConfig {
         template.setHashValueSerializer(genericJackson2JsonRedisSerializer);
         // 返回
         return template;
+    }
+
+    /**
+     * 定制Redis缓存管理器RedisCacheManager，实现自定义序列化并设置缓存时效
+     * @param redisConnectionFactory
+     * @return
+     */
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        // 分别创建String和JSON格式序列化对象，对缓存数据key和value进行转换
+        RedisSerializer<String> strSerializer = new StringRedisSerializer();
+        Jackson2JsonRedisSerializer jacksonSeial = new Jackson2JsonRedisSerializer(Object.class);
+        // 解决查询缓存转换异常的问题
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jacksonSeial.setObjectMapper(om);
+        // 定制缓存数据序列化方式及时效
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofDays(1))   // 设置缓存有效期为1天
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(strSerializer))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jacksonSeial))
+                .disableCachingNullValues();   // 对空数据不进行缓存
+        RedisCacheManager cacheManager = RedisCacheManager.builder(redisConnectionFactory).cacheDefaults(config).build();
+        return cacheManager;
     }
 }
--- sample-starter/src/test/java/com/hixtrip/sample/SampleApplicationTests.java
+++ sample-starter/src/test/java/com/hixtrip/sample/SampleApplicationTests.java
@@ -1,6 +1,9 @@
 package com.hixtrip.sample;
 
+import com.hixtrip.sample.infra.data.RedisUtils;
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -9,6 +12,19 @@ class SampleApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Autowired
+    private RedisUtils redisUtils;
+
+    @Test
+    public void saveValue() {
+        //存入Redis
+        redisUtils.set("username", "IT小辉同学");
+        System.out.println("保存成功！！！");
+        //根据key取出
+        String username = (String) redisUtils.get("username");
+        System.out.println("username="+username);
     }
 
 }
